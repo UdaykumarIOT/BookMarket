f68c69e8742fdb23c2543e1858f16ebb19207b54
Uday <udayjagan16392@gmail.com>
2025-05-05 21:52:37 +0530

    initial upload

--- BookMarket/obj/Debug/net9.0/BookMarket.AssemblyInfo.cs
+++ BookMarket/obj/Debug/net9.0/BookMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9844779ce79ae4ef4452343423979c85750cbc99")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
