b4eb7b0ea85dc026adee92af73afc349c5cd2cd6
UDAY KUMAR R <udayjagan16392@gmail.com>
2025-05-05 22:25:58 +0530

    second commit

--- BookMarket/obj/Debug/net9.0/BookMarket.AssemblyInfo.cs
+++ BookMarket/obj/Debug/net9.0/BookMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9844779ce79ae4ef4452343423979c85750cbc99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68c69e8742fdb23c2543e1858f16ebb19207b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
